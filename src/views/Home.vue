3793f56eacf6aba670e7359529452907d2bf5748
Meteorkun <33950309+Meteorkun@users.noreply.github.com>
2020-06-12 21:58:30 +0800

    主页分类组件初建

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,42 @@
 <template>
-  <div>
-    主页
+  <div class="home">
+    <nav-bar></nav-bar>
+    <div class="categorytab">
+      <van-tabs v-model="active" scrollspy sticky>
+        <van-tab v-for="(item,index) in category" :key="index" :title="item.title">
+          内容 {{ index }}
+        </van-tab>
+      </van-tabs>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/Navbar.vue'
 export default {
     data() {
         return {
-
+          category: [],
+          active: 0
         }
     },
     components:{
-
+      NavBar
     },
     methods:{
-
+      async selectCategory(){
+        const res = await this.$http.get("/category");
+        this.category = res.data
+      }
+    },
+    created(){
+      this.selectCategory();
     }
 }
 </script>
 
 <style lang="less">
-
+  .home{
+    background-color:white;
+  }
 </style>
