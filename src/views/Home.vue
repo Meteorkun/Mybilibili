63395b98f5d941efa3a219a3a16852ab8fba152e
Meteorkun <33950309+Meteorkun@users.noreply.github.com>
2020-06-20 21:30:22 +0800

    主页数据改造

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,13 +24,49 @@ export default {
       NavBar
     },
     methods:{
+      //搜索分类
       async selectCategory(){
         const res = await this.$http.get("/category");
-        this.category = res.data
+        this.category = this.changeCategory(res.data);
+      },
+      changeCategory(data){
+        // 重新改造分类数组
+        const category1 = data.map((item, index) => {
+          // list:每个分类中存储的文章数据
+          item.list = [];
+          item.page = 0;
+          item.finished = false;
+          item.loading = true;
+          item.pagesize = 10;
+          return item;
+        });
+          return category1;
+      },
+      // 获取文章数据
+      async selectArticle(){
+        //获取当前所点击的分类id
+        const categoryitem = this.categoryItem();
+        const res = await this.$http.get("/detail/" + categoryitem._id,{
+          //拼接url方法，把下面参数传递过去
+          params:{
+            page: categoryitem.page,
+            pagesize: categoryitem.pagesize
+          }
+        });
+      },
+      //获取当前所点击的分类id
+      categoryItem(){
+        const categoryItem = this.category[this.active];
+        return categoryItem;
       }
     },
     created(){
       this.selectCategory();
+    },
+    watch:{
+      active(){
+        this.selectArticle();
+      }
     }
 }
 </script>
